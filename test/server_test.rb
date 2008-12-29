cf1e398b42b1721e1887696b09b6f3f5fe9b2ee0
Nathan Fixler <nathan@fixler.org>
2008-12-29 14:37:45 -0500

    Test refactoring

--- test/server_test.rb
+++ test/server_test.rb
@@ -4,18 +4,24 @@ class MusicServerTest < Test::Unit::TestCase
   test 'get on /' do
     get_it '/'
     assert_equal 200, @response.status
-    assert_equal 'Foo', @response.body
+    assert_equal get_entries(MUSIC_BASE).to_s, @response.body
   end
   
   test 'get on /artist' do
     get_it '/Foo'
     assert_equal 200, @response.status
-    assert_equal 'Bar', @response.body
+    assert_equal get_entries(MUSIC_BASE + '/Foo').to_s, @response.body
+  end
+  
+  test 'get an /artist with funky characters' do
+    funky_uri = URI.escape '/Funky&'
+    get_it funky_uri
+    assert_equal 200, @response.status
+    assert_equal get_entries(MUSIC_BASE + URI.unescape(funky_uri)).to_s, @response.body
   end
   
   test 'get on /artist does not exist' do
     get_it '/nobody'
-    # assert_redirected '/'
     assert_equal 302, @response.status
     assert_equal '/', @response.headers['Location']
     assert_equal '', @response.body
@@ -24,7 +30,7 @@ class MusicServerTest < Test::Unit::TestCase
   test 'get on /artist/album' do
     get_it '/Foo/Bar'
     assert_equal 200, @response.status
-    assert_equal 'test.mp3', @response.body
+    assert_equal get_entries(MUSIC_BASE + '/Foo/Bar').to_s, @response.body
   end
   
   test 'get on /artist/album does not exist' do
@@ -43,5 +49,9 @@ class MusicServerTest < Test::Unit::TestCase
   test 'get on /artist/album/file.mp3 does not exist' do
     get_it '/Foo/Bar/Nothing.mp3'
     assert_equal '', @response.body
+  end
+  
+  def get_entries(path)
+    Dir.entries(path).reject {|e| e =~ /^\./}.sort
   end
 end
