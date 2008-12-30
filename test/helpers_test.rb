ba492ac0a79a434cfda6ac90884ad93d97777500
Nathan Fixler <nathan@fixler.org>
2008-12-30 13:37:21 -0500

    mock some directory entries, ya know

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -4,8 +4,8 @@ require 'mocha'
 class MusicHelpersTest < Test::Unit::TestCase
   def setup
     request = mock('request')
-    response = mock('response', :body= => nil)
-    route_params = mock('route_params')
+    response = mock('response', :body=)
+    route_params = {}
     @context = Sinatra::EventContext.new(request, response, route_params)
   end
 
@@ -15,17 +15,26 @@ class MusicHelpersTest < Test::Unit::TestCase
     assert_equal '/Foo/Bar/test.mp3', @context.url_for('Foo', 'Bar', 'test.mp3')
   end
 
-  test 'get_entries' do
-    assert_equal ['&,.#', 'Foo'], @context.get_entries(MUSIC_BASE + '/')
-    assert_equal ['Bar'], @context.get_entries(MUSIC_BASE + '/Foo')
-    assert_equal ['test.mp3'], @context.get_entries(MUSIC_BASE + '/Foo/Bar')
-    assert_nil @context.get_entries(MUSIC_BASE + '/').find {|e| e =~ /^\./ }
-    # 404
-    # sorted
+  test 'get_entries alpha sorts results' do
+    Dir.expects(:entries).returns(['c', 'b', 'a'])
+    assert_equal 'a', @context.get_entries('/').first
+  end
+  
+  test 'get_entries removes dotfiles' do
+    Dir.expects(:entries).returns(['a', '.a', '.', '..'])
+    entries = @context.get_entries('/')
+    assert_equal 1, entries.size
+    assert_equal ['a'], entries
+    assert_nil entries.find {|e| e =~ /^\./ }
+  end
+
+  test 'get_entries not found' do
+    # It should redirect if a path is not found
+    # @context.get_entries(MUSIC_BASE + '/Nothing')
   end
 
   test 'get_mp3' do
-    # return the contents from the mp3 path
+    assert_equal 'Hello world', @context.get_mp3(MUSIC_BASE + '/Foo/Bar/test.mp3')
     # halt if 404
   end
 
@@ -34,9 +43,18 @@ class MusicHelpersTest < Test::Unit::TestCase
     # return default album2.jpg if 404
   end
 
-  test 'title' do
-    # index
-    # artist
-    # artist : album
+  test 'title for index' do
+    @context.request.expects(:params).returns({})
+    assert_equal 'MUSIC', @context.title
+  end
+  
+  test 'title for artist' do
+    @context.request.expects(:params).returns({:artist => 'Foo'})
+    assert_equal 'MUSIC: Foo', @context.title
+  end
+
+  test 'title for artist/album' do
+    @context.request.expects(:params).returns({:artist => 'Foo', :album => 'Bar'})
+    assert_equal 'MUSIC: Foo: Bar', @context.title
   end
 end
