ff8af61688aa85e7bd3403c3f789e2e3f2824de6
Nathan Fixler <nathan@fixler.org>
2008-12-30 12:34:13 -0500

    figured out how to test Sinatra's helpers

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -1,15 +1,42 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
+require 'mocha'
 
 class MusicHelpersTest < Test::Unit::TestCase
-  before do
+  def setup
     request = mock('request')
-    response = mock('response', :body => nil)
+    response = mock('response', :body= => nil)
     route_params = mock('route_params')
-    @event_context = Sinatra::EventContext.new(request, response, route_params)
+    @context = Sinatra::EventContext.new(request, response, route_params)
   end
 
-  test 'url_for artist' do
-    puts @event_context.inspect
-    assert_equal '/Foo', @event_context.url_for("Foo")
+  test 'url_for' do
+    assert_equal '/Foo%20Bar', @context.url_for("Foo Bar")
+    assert_equal '/Foo/Bar', @context.url_for("Foo", "Bar")
+    assert_equal '/Foo/Bar/test.mp3', @context.url_for('Foo', 'Bar', 'test.mp3')
+  end
+
+  test 'get_entries' do
+    assert_equal ['&,.#', 'Foo'], @context.get_entries(MUSIC_BASE + '/')
+    assert_equal ['Bar'], @context.get_entries(MUSIC_BASE + '/Foo')
+    assert_equal ['test.mp3'], @context.get_entries(MUSIC_BASE + '/Foo/Bar')
+    assert_nil @context.get_entries(MUSIC_BASE + '/').find {|e| e =~ /^\./ }
+    # 404
+    # sorted
+  end
+
+  test 'get_mp3' do
+    # return the contents from the mp3 path
+    # halt if 404
+  end
+
+  test 'get_album_jpg' do
+    # return the contents of album.jpg
+    # return default album2.jpg if 404
+  end
+
+  test 'title' do
+    # index
+    # artist
+    # artist : album
   end
 end
